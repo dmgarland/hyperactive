627b9f5ba075bc62793e6e4b7eb43baa5febea4a
yossarian <yossarian@aktivix.org>
2008-03-05 01:22:03 +0000

    Moved some url helpers into the application helper so they're more widely accessible.

--- vendor/plugins/hyperactive/app/helpers/search_helper.rb
+++ vendor/plugins/hyperactive/app/helpers/search_helper.rb
@@ -1,19 +1,2 @@
-module SearchHelper
-  
-  def content_url_for(entity)
-    if entity.class == Article
-      article_url(entity)
-    elsif entity.class == Event
-      event_url(entity)
-    end
-  end
-  
-  def icon_image_for(entity)
-    if entity.class == Article
-      image_tag "icon_article.gif"
-    elsif entity.class == Event
-      image_tag "date.png"
-    end    
-  end
-  
+module SearchHelper  
 end

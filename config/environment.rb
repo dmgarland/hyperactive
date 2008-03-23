77fdb84662902cf27ebec058f77dd5a366e65dd1
yossarian <yossarian@aktivix.org>
2008-03-23 17:11:37 +0000

    commenting out this globalize preload code until i can figure out where to put it.

--- config/environment.rb
+++ config/environment.rb
@@ -107,14 +107,14 @@ end
 
 # In production mode, loads every view translation into memory once from the database
 # when the application starts, avoiding database calls from globalize.
-Dispatcher.to_prepare :globalize_view_translations do
-  if RAILS_ENV == 'production' 
-    translator = Globalize::DbViewTranslator.instance
-      #Or whichever other means used to keep track of app specific supported languages 
-      SupportedLanguage.find(:all).each do |lang|
-        ViewTranslation.find(:all, :conditions => ['globalize_translations.language_id = ?', lang.language_id]).each do |tr|
-          translator.send(:cache_add, tr.tr_key, tr.language, tr.pluralization_index, tr.text)
-        end  
-    end 
-  end
-end
+#Dispatcher.to_prepare :globalize_view_translations do
+#  if RAILS_ENV == 'production' 
+#    translator = Globalize::DbViewTranslator.instance
+#      #Or whichever other means used to keep track of app specific supported languages 
+#      SupportedLanguage.find(:all).each do |lang|
+#        ViewTranslation.find(:all, :conditions => ['globalize_translations.language_id = ?', lang.language_id]).each do |tr|
+#          translator.send(:cache_add, tr.tr_key, tr.language, tr.pluralization_index, tr.text)
+#        end  
+#    end 
+#  end
+#end

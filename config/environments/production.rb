6499b2cd68340bfa598cf9395a52e7d0953475f6
yossarian <yossarian@aktivix.org>
2008-03-24 23:49:52 +0000

    Setting up sendmail.  It's not working yet but these things take time sometimes.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -16,5 +16,6 @@ config.action_controller.perform_caching             = true
 
 # Disable delivery errors, bad email addresses will be ignored
 # config.action_mailer.raise_delivery_errors = false
+config.action_mailer.delivery_method = :sendmail
 
 config.plugins = ["engines", "*"]

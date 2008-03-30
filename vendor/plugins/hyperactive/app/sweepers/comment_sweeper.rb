e184ddc7020ae2e0a5e6e5eefd1509fda762d0f3
yossarian <yossarian@aktivix.org>
2008-03-30 19:12:08 +0000

    Setting up a comment_sweeper so that we don't expire the front page every time some idiot makes a comment.

---
+++ vendor/plugins/hyperactive/app/sweepers/comment_sweeper.rb
@@ -0,0 +1,21 @@
+class CommentSweeper < ActionController::Caching::Sweeper
+  
+  observe Comment
+  
+  def after_create(comment)
+    expire_cache_for(comment.content)
+  end
+  
+  def after_destroy(comment)
+    expire_cache_for(comment.content)
+  end
+  
+  private
+
+  # Expire the comment's associated content show page when a comment is made.
+  #
+  def expire_cache_for(content)
+    expire_page(:controller => content.class.to_s.downcase.pluralize, :action => 'show', :id => content.id)
+  end
+
+end

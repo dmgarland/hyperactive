2147232a2c73d679e173dbc86c67eb2527f3a5f9
yossarian <yossarian@aktivix.org>
2008-04-06 01:46:10 +0000

    Email config now working.

--- vendor/plugins/hyperactive/app/models/content_hide_mailer.rb
+++ vendor/plugins/hyperactive/app/models/content_hide_mailer.rb
@@ -1,29 +1,29 @@
 class ContentHideMailer < ActionMailer::Base
 
   def hide(content,reasons, user)
+    setup_email
     @subject    = 'Content Hidden'
     @body      = {:content => content, :reasons => reasons, :user => user}
-    @recipients = 'yossarian@aktivix.org'
-    @from       = 'site@ny.indymedia.dk'
-    @sent_on    = Time.now
-    @headers    = {}
   end
   
   def report(content,reasons, user)
+    setup_email    
     @subject    = 'Problem reported with content'
     @body      = {:content => content, :reasons => reasons, :user => user}
-    @recipients = 'yossarian@aktivix.org'
-    @from       = 'site@ny.indymedia.dk'
-    @sent_on    = Time.now
-    @headers    = {}
   end  
   
   def unhide(content,reasons, user)
+    setup_email    
     @subject    = 'Content Unhidden'
     @body       = {:content => content, :reasons => reasons, :user => user}
+  end
+  
+  private 
+  
+  def setup_email
     @recipients = 'yossarian@aktivix.org'
-    @from       = 'site@ny.indymedia.dk'
+    @from       = "indy site <site@ny.indymedia.dk>"
     @sent_on    = Time.now
-    @headers    = {}
+    @headers    = {}    
   end
 end

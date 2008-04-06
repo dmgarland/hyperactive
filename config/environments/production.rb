f81444aa433093b62747629561bfdd18c2cb9408
yossarian <yossarian@aktivix.org>
2008-04-06 01:42:38 +0000

    Adding config for email sending on debian.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -16,6 +16,15 @@ config.action_controller.perform_caching             = true
 
 # Disable delivery errors, bad email addresses will be ignored
 # config.action_mailer.raise_delivery_errors = false
-config.action_mailer.delivery_method = :sendmail
+# config.action_mailer.delivery_method = :sendmail
 
 config.plugins = ["engines", "*"]
+
+config.action_mailer.raise_delivery_errors = true
+
+ActionMailer::Base.delivery_method = :sendmail
+
+ActionMailer::Base.sendmail_settings = {
+  :location       => '/usr/sbin/sendmail',
+  :arguments      => '-i -t -f site@london.escapegoat.org'
+}

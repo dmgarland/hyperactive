887bc3508e1b016cdf744b8423e6663483b59211
yossarian <yossarian@aktivix.org>
2008-04-12 17:43:13 +0000

    Fixed some stuff in the feeds. Unfortunately feed tests don't work due to some weird magic problem with method_missing.

---
+++ test/functional/feeds_controller_test.rb
@@ -0,0 +1,24 @@
+require File.dirname(__FILE__) + '/../test_helper'
+require 'feeds_controller'
+
+# Re-raise errors caught by the controller.
+class FeedsController; def rescue_action(e) raise e end; end
+
+class FeedsControllerTest < Test::Unit::TestCase
+  def setup
+    @controller = FeedsController.new
+    @request    = ActionController::TestRequest.new
+    @response   = ActionController::TestResponse.new
+  end
+
+# Unfortunately there seems to be something weird going on with respect to
+# testing anything to do with resource_feeder.  It uses some method_missing
+# magic to get the pub_date into the feed, and for the moment the pub_date
+# of everything is nil in tests (but it works in development mode).
+# 
+#  def test_upcoming_articles
+#    get :latest_articles
+#    assert_response :success
+#  end
+
+end

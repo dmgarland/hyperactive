dba2af43c0d0f76a8b76e3d819f9b0a5ffb8020b
yossarian <yossarian@aktivix.org>
2008-04-30 17:40:07 +0000

    Wiping the active rbac stuff out of the initial migration for hyperactive, since it has its own plugin migration setup.  This should ease the pain of initial project setup.

--- vendor/plugins/hyperactive/db/migrate/008_initial_schema.rb
+++ vendor/plugins/hyperactive/db/migrate/008_initial_schema.rb
@@ -43,33 +43,6 @@ class InitialSchema < ActiveRecord::Migration
   
     add_index "file_uploads", ["event_id"], :name => "fk_file_upload_event"
   
-    create_table "groups", :force => false do |t|
-      t.column "created_at", :timestamp
-      t.column "updated_at", :timestamp
-      t.column "title", :string, :limit => 200, :default => "", :null => false
-      t.column "parent_id", :integer, :limit => 10
-    end
-  
-    add_index "groups", ["parent_id"], :name => "groups_parent_id_index"
-  
-    create_table "groups_roles", :id => false, :force => false do |t|
-      t.column "group_id", :integer, :limit => 10, :default => 0, :null => false
-      t.column "role_id", :integer, :limit => 10, :default => 0, :null => false
-      t.column "created_at", :timestamp
-    end
-  
-    add_index "groups_roles", ["group_id", "role_id"], :name => "groups_roles_all_index", :unique => true
-    add_index "groups_roles", ["role_id"], :name => "role_id"
-  
-    create_table "groups_users", :id => false, :force => false do |t|
-      t.column "group_id", :integer, :limit => 10, :default => 0, :null => false
-      t.column "user_id", :integer, :limit => 10, :default => 0, :null => false
-      t.column "created_at", :timestamp
-    end
-  
-    add_index "groups_users", ["group_id", "user_id"], :name => "groups_users_all_index", :unique => true
-    add_index "groups_users", ["user_id"], :name => "user_id"
-  
     create_table "links", :force => false do |t|
       t.column "title", :string, :default => "", :null => false
       t.column "url", :string, :default => "", :null => false
@@ -96,64 +69,6 @@ class InitialSchema < ActiveRecord::Migration
   
     add_index "photos", ["event_id"], :name => "fk2_photo_event"
   
-    create_table "roles", :force => false do |t|
-      t.column "created_at", :timestamp
-      t.column "updated_at", :timestamp
-      t.column "title", :string, :limit => 100, :default => "", :null => false
-      t.column "parent_id", :integer, :limit => 10
-    end
-  
-    add_index "roles", ["parent_id"], :name => "roles_parent_id_index"
-  
-    create_table "roles_static_permissions", :id => false, :force => false do |t|
-      t.column "role_id", :integer, :limit => 10, :default => 0, :null => false
-      t.column "static_permission_id", :integer, :limit => 10, :default => 0, :null => false
-      t.column "created_at", :timestamp
-    end
-  
-    add_index "roles_static_permissions", ["static_permission_id", "role_id"], :name => "roles_static_permissions_all_index", :unique => true
-  
-    create_table "roles_users", :id => false, :force => false do |t|
-      t.column "user_id", :integer, :limit => 10, :default => 0, :null => false
-      t.column "role_id", :integer, :limit => 10, :default => 0, :null => false
-      t.column "created_at", :timestamp
-    end
-  
-    add_index "roles_users", ["user_id", "role_id"], :name => "roles_users_all_index", :unique => true
-  
-    create_table "static_permissions", :force => false do |t|
-      t.column "title", :string, :limit => 200, :default => "", :null => false
-      t.column "created_at", :timestamp
-      t.column "updated_at", :timestamp
-    end
-  
-    add_index "static_permissions", ["title"], :name => "static_permissions_title_index", :unique => true
-   
-    create_table "user_registrations", :force => false do |t|
-      t.column "user_id", :integer, :limit => 10, :default => 0, :null => false
-      t.column "token", :text, :default => "", :null => false
-      t.column "created_at", :timestamp
-      t.column "expires_at", :timestamp
-    end
-  
-    add_index "user_registrations", ["user_id"], :name => "user_registrations_user_id_index", :unique => true
-    add_index "user_registrations", ["expires_at"], :name => "user_registrations_expires_at_index"
-  
-    create_table "users", :force => false do |t|
-      t.column "created_at", :timestamp
-      t.column "updated_at", :timestamp
-      t.column "last_logged_in_at", :timestamp
-      t.column "login_failure_count", :integer, :limit => 10, :default => 0, :null => false
-      t.column "login", :string, :limit => 100, :default => "", :null => false
-      t.column "email", :string, :limit => 200, :default => "", :null => false
-      t.column "password", :string, :limit => 100, :default => "", :null => false
-      t.column "password_hash_type", :string, :limit => 20, :default => "", :null => false
-      t.column "password_salt", :string, :limit => 10, :default => "1234512345", :null => false
-      t.column "state", :integer, :limit => 10, :default => 1, :null => false
-    end
-  
-    add_index "users", ["login"], :name => "users_login_index", :unique => true
-    add_index "users", ["password"], :name => "users_password_index"
   
   end
 
@@ -162,17 +77,8 @@ class InitialSchema < ActiveRecord::Migration
     drop_table :categories_events
     drop_table :events
     drop_table :file_uploads
-    drop_table :groups
-    drop_table :groups_roles
-    drop_table :groups_users
     drop_table :links
     drop_table :pages
     drop_table :photos
-    drop_table :roles
-    drop_table :roles_static_permissions
-    drop_table :roles_users
-    drop_table :static_permissions
-    drop_table :user_registrations
-    drop_table :users
   end
 end

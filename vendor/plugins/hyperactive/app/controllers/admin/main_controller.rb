e8a9cd3aabb6250f5922ed951ba517b9c230b72f
yossarian <yossarian@aktivix.org>
2008-05-24 23:23:56 +0000

    Comment editing now working.

--- vendor/plugins/hyperactive/app/controllers/admin/main_controller.rb
+++ vendor/plugins/hyperactive/app/controllers/admin/main_controller.rb
@@ -1,12 +1,28 @@
 class Admin::MainController < ApplicationController
 
   layout "admin"
+  
+  cache_sweeper :comment_sweeper, :only => [:update_comment]
 
   def index
   end
 
   def latest_comments
     @comments = Comment.find(:all, :limit => 20, :order => 'created_on DESC', :page => {:size => objects_per_page, :current => page_param})
+  end
+  
+  def edit_comment
+    @comment = Comment.find(params[:id])
+  end
+  
+  def update_comment
+    @comment = Comment.find(params[:id])
+    @comment.update_attributes(params[:comment])
+    if @comment.save
+      redirect_to :action => 'latest_comments'
+    else
+      render :action => 'edit_comment'
+    end
   end
   
   before_filter :protect_controller

cb468d3ca9b700bccbffbc8ab3f72fe26021be4d
yossarian <yossarian@aktivix.org>
2008-06-08 14:06:34 +0000

    When backgroundrb starts it now adds all torrent files to the transmission session.

--- lib/workers/torrent_worker.rb
+++ lib/workers/torrent_worker.rb
@@ -16,7 +16,8 @@ class TorrentWorker < BackgrounDRb::Rails
  #
   def do_work
     puts "@transmission object initialized"
-    @transmission = Transmission.new      
+    @transmission = Transmission.new
+    find_and_add_all_torrent_files
   end
 
   # Add a torrent download to the Transmission session
@@ -24,7 +25,8 @@ class TorrentWorker < BackgrounDRb::Rails
   def add_torrent(file)
     begin
       torrent_download = @transmission.open(file)
-      puts "Torrent theoretically added."
+      torrent_download.folder = File.dirname(file) +"/"
+      puts "Torrent theoretically added, downloading to: #{torrent_download.folder}"
       torrent_download.start
       puts "Torrent: #{torrent_download.name} started."
     rescue Exception => e
@@ -40,12 +42,17 @@ class TorrentWorker < BackgrounDRb::Rails
     puts "Torrent stopped.\n:#{torrent} "
   end
   
+  # Start a torrent
+  #
   def start_torrent(hash_string)
     torrent = get_torrent(hash_string)
     torrent.start
     puts "Torrent started. \n #{torrent}"
   end
   
+  # Return an array of all torrents which transmission currently 
+  # knows about
+  #
   def list_all_torrents
     torrents = []
     @transmission.each do |torrent|
@@ -55,6 +62,8 @@ class TorrentWorker < BackgrounDRb::Rails
     return torrents
   end
   
+  # Return an array of all torrents which are currently active
+  #
   def list_active_torrents
     torrents = []
     @transmission.each do |torrent|
@@ -63,9 +72,19 @@ class TorrentWorker < BackgrounDRb::Rails
     return torrents    
   end
   
+  # Return a reference to a torrent object so we can play with it.
+  # 
   def get_torrent(hash_string)
     @transmission.each do |torrent|
       return torrent if torrent.hash_string == hash_string
+    end
+  end
+
+  private
+  
+  def find_and_add_all_torrent_files
+    Dir['**/*.torrent'].each do |path|
+      add_torrent(path)
     end
   end
   

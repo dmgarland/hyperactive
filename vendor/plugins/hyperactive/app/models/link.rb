e077416d7360c18536dacfc466d379a61fa3e9ac
yossarian <yossarian@aktivix.org>
2008-07-05 13:08:42 +0000

    Taking a simplistic crack at fixing the XSS vulnerability in links.

--- vendor/plugins/hyperactive/app/models/link.rb
+++ vendor/plugins/hyperactive/app/models/link.rb
@@ -4,5 +4,11 @@ class Link < ActiveRecord::Base
   def after_initialize
     self.url = "http://" if self.url == ""
   end
+  
+  def before_save
+    if self.url =~ /javascript:/
+      self.url = "http://ha.ckers.org/xss.html"
+    end
+  end
 
 end

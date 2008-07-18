16cf77487189b38a3fe23bb5f720b8c510944126
yossarian <yossarian@aktivix.org>
2008-07-18 15:39:05 +0000

    The date-path stuff for ticket:167 is sort of working, it's pointing outside public/system at the moment, though, so will still need to be cleaned up.

--- vendor/plugins/hyperactive/app/models/photo.rb
+++ vendor/plugins/hyperactive/app/models/photo.rb
@@ -6,21 +6,23 @@ class Photo < ActiveRecord::Base
   
   image_column  :file, 
                 :versions => { :thumb => "100x100", :big_thumb => "180x400", :medium => "480x480" },
-                :root_path => File.join(RAILS_ROOT, "public/system/"),
-                :web_root => 'system',
-                :extensions => ["gif", "png", "jpg"]
+                :extensions => ["gif", "png", "jpg"]    
 
-# eventually we'll want: 
-#
-#              :root_path => File.join(RAILS_ROOT, "public/system/#{self.date_path}"), 
-
-  
-  def date_path
-    if self.new_record?
-      return self.created_on.strftime("%Y/%m/%d") 
+  # A callback method which determines the path where images will be stored.  
+  # Upload_column automatically uses this method to figure out the directory path to create for 
+  # each uploaded image.  
+  # 
+  # This approach splits up image uploads by date, so that we don't put thousands of images in the same directory
+  # and end up running out of file descriptors.
+  #   
+  def file_store_dir
+    if !self.created_on.nil?
+      return self.created_on.strftime("system/photo/%Y/%m/%d/") +  self.id.to_s
     else
-      return Date.today.strftime("%Y/%m/%d")
+      return Date.today.strftime("system/photo/%Y/%m/%d/") +  self.id.to_s
     end
   end
+  
+
     
 end

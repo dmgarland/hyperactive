bfbe7c24e5b54fe2c4d149ac0107cf045e9efece
yossarian <yossarian@aktivix.org>
2008-07-18 15:58:32 +0000

    Ok I think this is it for the Photo date-path stuff (ticket:167).  Now I just need to write some sort of script to convert the Denmark site from the old image paths to the new image paths (and probably do Video and !FileUpload) and we are in business.

--- vendor/plugins/hyperactive/app/models/photo.rb
+++ vendor/plugins/hyperactive/app/models/photo.rb
@@ -6,7 +6,9 @@ class Photo < ActiveRecord::Base
   
   image_column  :file, 
                 :versions => { :thumb => "100x100", :big_thumb => "180x400", :medium => "480x480" },
-                :extensions => ["gif", "png", "jpg"]    
+                :extensions => ["gif", "png", "jpg"],
+                :root_path => File.join(RAILS_ROOT, "public", "system"),
+                :web_root => "/system"
 
   # A callback method which determines the path where images will be stored.  
   # Upload_column automatically uses this method to figure out the directory path to create for 
@@ -17,9 +19,9 @@ class Photo < ActiveRecord::Base
   #   
   def file_store_dir
     if !self.created_on.nil?
-      return self.created_on.strftime("system/photo/%Y/%m/%d/") +  self.id.to_s
+      return self.created_on.strftime("photo/%Y/%m/%d/") +  self.id.to_s
     else
-      return Date.today.strftime("system/photo/%Y/%m/%d/") +  self.id.to_s
+      return Date.today.strftime("photo/%Y/%m/%d/") +  self.id.to_s
     end
   end
   

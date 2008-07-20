586c0f61ec9641e007a6c25eb56192ae9066b2b1
yossarian <yossarian@aktivix.org>
2008-07-20 23:51:51 +0000

    fixed bad file paths in migration.

--- db/migrate/016_move_photos_to_new_file_paths.rb
+++ db/migrate/016_move_photos_to_new_file_paths.rb
@@ -3,20 +3,20 @@ class MovePhotosToNewFilePaths < ActiveRecord::Migration
     photos = Photo.find(:all)
     photos.each do |photo|
       from_dir = "#{File.expand_path(RAILS_ROOT)}/public/system/photo/file/#{photo.id.to_s}"
-      from = "#{File.expand_path(RAILS_ROOT)}/public/system/photo/file/#{photo.id.to_s}/#{photo.file}"
+      from = "#{File.expand_path(RAILS_ROOT)}/public/system/photo/file/#{photo.id.to_s}/#{photo.file.filename}"
       if File.exists?(from)
         to_dir = Date.today.strftime("#{File.expand_path(RAILS_ROOT)}/public/system/photo/%Y/%m/%d/#{photo.id.to_s}")
-        to = Date.today.strftime("#{File.expand_path(RAILS_ROOT)}/public/system/photo/%Y/%m/%d/#{photo.id.to_s}/#{photo.file}")
+        to = Date.today.strftime("#{File.expand_path(RAILS_ROOT)}/public/system/photo/%Y/%m/%d/#{photo.id.to_s}/#{photo.file.filename}")
         FileUtils.mkdir_p(to_dir)
         FileUtils.cp(from, to)
-        if File.exists?("#{from_dir}/medium/#{photo.file}")
-          FileUtils.cp("#{from_dir}/medium/#{photo.file}", "#{to_dir}/#{photo.file.filename_base}-medium.#{photo.file.filename_extension}")
+        if File.exists?("#{from_dir}/medium/#{photo.file.filename}")
+          FileUtils.cp("#{from_dir}/medium/#{photo.file.filename}", "#{to_dir}/#{photo.file.filename_base}-medium.#{photo.file.filename_extension}")
         end
-        if File.exists?("#{from_dir}/thumb/#{photo.file}")
-          FileUtils.cp("#{from_dir}/thumb/#{photo.file}", "#{to_dir}/#{photo.file.filename_base}-thumb.#{photo.file.filename_extension}")
+        if File.exists?("#{from_dir}/thumb/#{photo.file.filename}")
+          FileUtils.cp("#{from_dir}/thumb/#{photo.file.filename}", "#{to_dir}/#{photo.file.filename_base}-thumb.#{photo.file.filename_extension}")
         end
-        if File.exists?("#{from_dir}/big_thumb/#{photo.file}")
-          FileUtils.cp("#{from_dir}/big_thumb/#{photo.file}", "#{to_dir}/#{photo.file.filename_base}-big_thumb.#{photo.file.filename_extension}")
+        if File.exists?("#{from_dir}/big_thumb/#{photo.file.filename}")
+          FileUtils.cp("#{from_dir}/big_thumb/#{photo.file.filename}", "#{to_dir}/#{photo.file.filename_base}-big_thumb.#{photo.file.filename_extension}")
         end
         photo.created_on = Date.today
         photo.save

efbfe3caa588517c75a7875dafca76f23d6dd946
yossarian <yossarian@aktivix.org>
2008-07-25 22:59:04 +0000

    Allowing direct HTML code viewing in the page controller in admin.

--- vendor/plugins/hyperactive/app/controllers/admin/page_controller.rb
+++ vendor/plugins/hyperactive/app/controllers/admin/page_controller.rb
@@ -15,7 +15,7 @@ class Admin::PageController < ApplicationController
                            :theme_advanced_resize_horizontal => false,
                            :theme_advanced_resizing_use_cookie => true,
                            :paste_auto_cleanup_on_paste => true,
-                           :theme_advanced_buttons1 => %w{undo redo separator bold italic underline strikethrough separator bullist numlist separator link unlink separator cleanup},
+                           :theme_advanced_buttons1 => %w{undo redo separator bold italic underline strikethrough separator bullist numlist separator link unlink separator cleanup code},
                            :theme_advanced_buttons2 => [],
                            :theme_advanced_buttons3 => [],
                            :plugins => %w{paste cleanup}},

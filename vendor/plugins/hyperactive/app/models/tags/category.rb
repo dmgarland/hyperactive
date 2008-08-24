6f8bb594519aeb37ab025e01ad6eb5e2a31e99de
yossarian <yossarian@aktivix.org>
2008-08-24 21:04:30 +0000

    Ok things are really screwed now.  Seeing if this does anything.

--- vendor/plugins/hyperactive/app/models/tags/category.rb
+++
@@ -1,5 +0,0 @@
-class Category < ActiveRecord::Base
-  validates_presence_of :name, :description, :active
-  validates_length_of :name, :minimum=>3
-  validates_length_of :description, :minimum=>10
-end
--- vendor/plugins/hyperactive/init.rb
+++ vendor/plugins/hyperactive/init.rb
@@ -4,7 +4,7 @@
 Rails.plugins[:hyperactive].code_paths << "app/sweepers"
 
 # Load up the model subfolders when hyperactive starts
-Rails.plugins[:hyperactive].code_paths << "app/models/active_rbac"
-Rails.plugins[:hyperactive].code_paths << "app/models/collectives"
-Rails.plugins[:hyperactive].code_paths << "app/models/content"
-Rails.plugins[:hyperactive].code_paths << "app/models/tags"
+#Rails.plugins[:hyperactive].code_paths << "app/models/active_rbac"
+#Rails.plugins[:hyperactive].code_paths << "app/models/collectives"
+#Rails.plugins[:hyperactive].code_paths << "app/models/content"
+#Rails.plugins[:hyperactive].code_paths << "app/models/tags"

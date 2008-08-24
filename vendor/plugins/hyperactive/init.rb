7c8b047623f10e9927e98bf286bb748127714e9c
yossarian <yossarian@aktivix.org>
2008-08-24 01:58:32 +0000

    Moved a whole bunch of models around so that they're grouped in folders, things were getting a little ugly.

--- vendor/plugins/hyperactive/init.rb
+++ vendor/plugins/hyperactive/init.rb
@@ -2,4 +2,9 @@
 
 # Load up the cache sweepers when hyperactive starts
 Rails.plugins[:hyperactive].code_paths << "app/sweepers"
+
+# Load up the model subfolders when hyperactive starts
 Rails.plugins[:hyperactive].code_paths << "app/models/active_rbac"
+Rails.plugins[:hyperactive].code_paths << "app/models/collectives"
+Rails.plugins[:hyperactive].code_paths << "app/models/content"
+Rails.plugins[:hyperactive].code_paths << "app/models/tags"

59b79530922ded86ad86c4b78e4f23aef6ebc49d
yossarian <yossarian@aktivix.org>
2008-08-25 22:56:49 +0000

    A version of has_many_polymorphs that works with Rails 2.1/

--- vendor/plugins/has_many_polymorphs/lib/has_many_polymorphs/autoload.rb
+++ vendor/plugins/has_many_polymorphs/lib/has_many_polymorphs/autoload.rb
@@ -1,5 +1,6 @@
 
-require 'dispatcher'
+require 'initializer' unless defined? ::Rails::Initializer 
+require 'dispatcher' unless defined? ::ActionController::Dispatcher
 
 module HasManyPolymorphs
 
@@ -11,52 +12,59 @@ Note that you can override DEFAULT_OPTIONS via Rails::Configuration#has_many_pol
     # your other configuration here
     
     config.after_initialize do
-      config.has_many_polymorphs_options['requirements'] << '/lib/my_extension'
+      config.has_many_polymorphs_options['requirements'] << 'lib/my_extension'
     end    
   end
   
 =end
 
-  module Autoload
-        
-    DEFAULT_OPTIONS = {
-      :file_pattern => "#{RAILS_ROOT}/app/models/**/*.rb",
-      :file_exclusions => ['svn', 'CVS', 'bzr'],
-      :methods => ['has_many_polymorphs', 'acts_as_double_polymorphic_join'],
-      :requirements => []}
-    
-    mattr_accessor :options
-    @@options = HashWithIndifferentAccess.new(DEFAULT_OPTIONS)      
+  DEFAULT_OPTIONS = {
+    :file_pattern => "#{RAILS_ROOT}/app/models/**/*.rb",
+    :file_exclusions => ['svn', 'CVS', 'bzr'],
+    :methods => ['has_many_polymorphs', 'acts_as_double_polymorphic_join'],
+    :requirements => []}
+  
+  mattr_accessor :options
+  @@options = HashWithIndifferentAccess.new(DEFAULT_OPTIONS)      
 
-    # Dispatcher callback to load polymorphic relationships from the top down.
-    def self.autoload
+  # Dispatcher callback to load polymorphic relationships from the top down.
+  def self.autoload
 
-      _logger_debug "autoload hook invoked"
-      
-      options[:requirements].each do |requirement|
-        require requirement
-      end
+    _logger_debug "autoload hook invoked"
     
-      Dir[options[:file_pattern]].each do |filename|
-        next if filename =~ /#{options[:file_exclusions].join("|")}/
-        open filename do |file|
-          if file.grep(/#{options[:methods].join("|")}/).any?
-            begin
-              model = File.basename(filename)[0..-4].camelize
-              _logger_warn "preloading parent model #{model}"
-              model.constantize
-            rescue Object => e
-              _logger_warn "WARNING; possibly critical error preloading #{model}: #{e.inspect}"
-            end
+    options[:requirements].each do |requirement|
+      _logger_warn "forcing requirement load of #{requirement}"
+      require requirement
+    end
+  
+    Dir[options[:file_pattern]].each do |filename|
+      next if filename =~ /#{options[:file_exclusions].join("|")}/
+      open filename do |file|
+        if file.grep(/#{options[:methods].join("|")}/).any?
+          begin
+            model = File.basename(filename)[0..-4].camelize
+            _logger_warn "preloading parent model #{model}"
+            model.constantize
+          rescue Object => e
+            _logger_warn "#{model} could not be preloaded: #{e.inspect}"
           end
         end
       end
-    end  
-    
-  end
+    end
+  end  
     
 end
 
-Dispatcher.to_prepare do
-  HasManyPolymorphs::Autoload.autoload
+class Rails::Initializer #:nodoc:
+  # Make sure it gets loaded in the console, tests, and migrations
+  def after_initialize_with_autoload 
+    after_initialize_without_autoload
+    HasManyPolymorphs.autoload
+  end
+  alias_method_chain :after_initialize, :autoload 
+end
+
+Dispatcher.to_prepare(:has_many_polymorphs_autoload) do
+  # Make sure it gets loaded in the app
+  HasManyPolymorphs.autoload
 end

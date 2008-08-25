a7b95d27632d9b8947a9a37f9bfc2367007179d6
yossarian <yossarian@aktivix.org>
2008-08-25 22:37:20 +0000

    Commented out the cache sweeper loading code in the hyperactive plugin, this should be fixed though.  See the note in the file.

--- vendor/plugins/hyperactive/init.rb
+++ vendor/plugins/hyperactive/init.rb
@@ -1,10 +1,8 @@
 # Include hook code here
 
 # Load up the cache sweepers when hyperactive starts
-Rails.plugins[:hyperactive].code_paths << "app/sweepers"
-
-# Load up the model subfolders when hyperactive starts
-#Rails.plugins[:hyperactive].code_paths << "app/models/active_rbac"
-#Rails.plugins[:hyperactive].code_paths << "app/models/collectives"
-#Rails.plugins[:hyperactive].code_paths << "app/models/content"
-#Rails.plugins[:hyperactive].code_paths << "app/models/tags"
+#
+# TODO 2.1 : This next line isn't working. I've stuck an equivalent bit of code into 
+# Environment.rb instead but that's the wrong place for it really...
+#
+#Rails.plugins[:hyperactive].code_paths << "app/sweepers"

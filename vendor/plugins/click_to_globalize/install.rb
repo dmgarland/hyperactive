1094ea9d9bd06bd580e06e4e228e57c8d760987f
yossarian <yossarian@aktivix.org>
2008-08-26 01:15:03 +0000

    Click to globalize is screwing up plugin loads.

--- vendor/plugins/click_to_globalize/install.rb
+++
@@ -1,4 +0,0 @@
-puts "Click To Globalize was correctly downloaded.\n"+
-     "To complete the installation run:\n"+
-     "\trake click:setup\n\n"+
-     "Click To Globalize will install Prototype 1.5.1.1, please backup your scripts."

9653c1ac6d8a05c841e42088374702f1660f3db7
yossarian <yossarian@aktivix.org>
2008-09-28 14:09:14 +0000

    unpacked rails 2.1.0 and mislav-will_paginate

--- config/environment.rb
+++ config/environment.rb
@@ -71,7 +71,7 @@ end
 
 # Add new inflection rules using the following format 
 # (all these examples are active by default):
-ActiveSupport::Inflector.inflections do |inflect|
+Inflector.inflections do |inflect|
 #   inflect.plural /^(ox)$/i, '\1en'
 #   inflect.singular /^(ox)en/i, '\1'
 #   inflect.irregular 'person', 'people'

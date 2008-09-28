4edbc35782b2e5cc5c56dcf8dfecf64e2cd1098c
yossarian <yossarian@aktivix.org>
2008-09-28 12:26:51 +0000

    Got rid of a deprecation warning related to Inflector by changine it to ActiveSuppoer::Inflector which I guess is the new styles.

--- config/environment.rb
+++ config/environment.rb
@@ -69,7 +69,7 @@ end
 
 # Add new inflection rules using the following format 
 # (all these examples are active by default):
-Inflector.inflections do |inflect|
+ActiveSupport::Inflector.inflections do |inflect|
 #   inflect.plural /^(ox)$/i, '\1en'
 #   inflect.singular /^(ox)en/i, '\1'
 #   inflect.irregular 'person', 'people'

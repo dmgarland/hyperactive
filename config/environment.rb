c9313518d83ebf8b8ce9450b67b7aa38e7743179
yossarian <yossarian@aktivix.org>
2008-09-28 13:58:52 +0000

    Jesus christ. Moving back to Rails 2.1.0 which unpacks gems properly.

--- config/environment.rb
+++ config/environment.rb
@@ -5,7 +5,7 @@
 # ENV['RAILS_ENV'] ||= 'production'
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.1.1' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.1.0' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
@@ -65,7 +65,7 @@ Rails::Initializer.run do |config|
   # Hyperactive plugin.
   #config.reload_plugins = true
 
-  config.gem 'mislav-will_paginate', :version => '~> 2.3.4', :lib => 'will_paginate', :source => 'http://gems.github.com'
+  config.gem 'mislav-will_paginate', :version => '2.3.4', :lib => 'will_paginate', :source => 'http://gems.github.com'
 
 end
 

6afcc141e80e2f59c5dea64c29627594d18f317c
yossarian <yossarian@aktivix.org>
2008-10-05 14:26:06 +0000

    Code cleanup

--- vendor/plugins/hyperactive/app/controllers/active_rbac/component_controller.rb
+++ vendor/plugins/hyperactive/app/controllers/active_rbac/component_controller.rb
@@ -1,23 +1,27 @@
 # Mix in the a before filter into ActiveRbac::ComponentController to secure
 # all ActiveRbac controllers.
 class ActiveRbac::ComponentController < ApplicationController
+  
+  require_dependency 'vendor/plugins/active_rbac/app/controllers/active_rbac/component_controller'
   before_filter :protect_with_active_rbac
 
   include SslRequirement
   ssl_required :all 
   
   protected
-    def protect_with_active_rbac
-      # only protect certain controllers
-      return true if [ActiveRbac::LoginController,
-          ActiveRbac::RegistrationController].include?(self.class)
-      # protect!
-      return true if not current_user.nil?  and
-          current_user.has_role? 'Admin'
-      flash[:notice] = 'You have insufficient permissions!'
-      redirect_to '/login'
-      return false
-    end
-    def protect_me
-    end
+  
+  def protect_with_active_rbac
+    # only protect certain controllers
+    return true if [ActiveRbac::LoginController, ActiveRbac::RegistrationController].include?(self.class)
+    # protect!
+    return true if not current_user.nil?  and
+        current_user.has_role? 'Admin'
+    flash[:notice] = 'You have insufficient permissions!'
+    redirect_to '/login'
+    return false
+  end
+
+  def protect_me
+  end
+
 end

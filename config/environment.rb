9c0ea5ae4302f6b3d60b86aba6b1abd22fc201ec
yossarian <yossarian@aktivix.org>
2008-10-05 18:45:40 +0000

    Ok so this is the new-look application.

--- config/environment.rb
+++ config/environment.rb
@@ -20,7 +20,7 @@ Rails::Initializer.run do |config|
   # Only load the plugins named here, in the order given. By default, all plugins 
   # in vendor/plugins are loaded in alphabetical order.
   # :all can be used as a placeholder for all plugins not explicitly named
-  config.plugins = [:engines, :active_rbac, :white_list, :sanitize_params, :hyperactive, :all] 
+  # config.plugins = [:engines, :active_rbac, :white_list, :sanitize_params, :hyperactive, :all] 
 
   # Add additional load paths for your own custom dirs
   # config.load_paths += %W( #{RAILS_ROOT}/extras )

ac912875b0b7526168425b61834b568853ed6a81
yossarian <yossarian@aktivix.org>
2008-10-11 18:41:45 +0000

    The "active" tickboxes aren't working in the categories admin due to the fact that the tickbox submits a blank on the form if it's not ticked.  Let's see what happens if we remove the validation on this.

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,5 @@
 class Category < ActiveRecord::Base
-  validates_presence_of :name, :description, :active
+  validates_presence_of :name, :description
   validates_length_of :name, :minimum=>3
   validates_length_of :description, :minimum=>10
 end

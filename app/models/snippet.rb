d6865aea8e11d3e3ccecdc00cbaf92ada4aa1485
yossarian <yossarian@aktivix.org>
2008-10-17 19:53:42 +0000

    Knocking the snippet size back to 100 pixels square.

--- app/models/snippet.rb
+++ app/models/snippet.rb
@@ -1,7 +1,7 @@
 class Snippet < ActiveRecord::Base
   
   image_column  :image, 
-              :versions => { :thumb => "c120x120" },
+              :versions => { :thumb => "c100x100" },
               :extensions => ["gif", "png", "jpg"],
               :root_dir => File.join(RAILS_ROOT, "public", "system"),
               :web_root => "/system"

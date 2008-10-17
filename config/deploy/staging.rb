094db03ecec8e31ebd0bc8587090b164a186a2df
yossarian <yossarian@aktivix.org>
2008-10-17 19:05:45 +0000

    New deployment arrangements.

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,5 +1,5 @@
-set :deploy_to, "/var/www/londonstaging.escapegoat.org/"
+set :deploy_to, "/home/yossarian/www/london.indymedia.org.uk/"
 
-role :web, "londonstaging.escapegoat.org"
-role :app, "londonstaging.escapegoat.org"
-role :db,  "londonstaging.escapegoat.org", :primary => true
+role :web, "london.indymedia.org.uk"
+role :app, "london.indymedia.org.uk"
+role :db,  "london.indymedia.org.uk", :primary => true

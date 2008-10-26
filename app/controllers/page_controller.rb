515c740ac0180e026b9b8f24e859643b81ea4668
yossarian <yossarian@aktivix.org>
2008-10-26 00:27:45 +0000

    Killing annoying app errors for pages, raising ActiveRecord::RecordNotFound will generate a 404 instead of a 500 in production mode.

--- app/controllers/page_controller.rb
+++ app/controllers/page_controller.rb
@@ -3,7 +3,8 @@ class PageController < ApplicationController
   layout "two_column"
 
   def show
-    @page = Page.find_by_title(params['title'])
+    @page = Page.find_by_title(params[:title])
+    raise ActiveRecord::RecordNotFound if @page == nil
   end
   
 end

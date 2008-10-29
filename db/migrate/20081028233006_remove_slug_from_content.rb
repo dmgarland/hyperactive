aab995fabb5818546730f7f89c815b6f08af48bc
yossarian <yossarian@aktivix.org>
2008-10-29 00:01:05 +0000

    Ditching the Slugger stuff, it's not needed and is screwing up migrations on the production site.

--- db/migrate/20081028233006_remove_slug_from_content.rb
+++
@@ -1,9 +0,0 @@
-class RemoveSlugFromContent < ActiveRecord::Migration
-  def self.up
-    remove_column :collectives, :slug
-  end
-
-  def self.down
-    add_column :collectives, :slug, :string
-  end
-end

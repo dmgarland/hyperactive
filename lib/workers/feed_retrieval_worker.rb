ce77dc6464c846616965ce8dfbdbfd8fb5589cf0
yossarian <yossarian@aktivix.org>
2008-11-01 15:40:16 +0000

    Refactored the feed retrieval worker so that it can be used to pull feeds from arbitrary locations and cache them in Hyperactive's cache directory.

--- lib/workers/feed_retrieval_worker.rb
+++ lib/workers/feed_retrieval_worker.rb
@@ -20,21 +20,50 @@ class FeedRetrievalWorker < BackgrounDRb::Rails
   # This method is called in it's own new thread when you
   # call new worker. args is set to :args  
   def do_work(args)
+    pull_uk_promoted_feed
+    pull_group_feeds
+  end
+  
+  
+  private 
+  
+  # Pulls an RSS or Atom feed from a given url and caches it as a series of list items on disk
+  # at the given cache_path.
+  #
+  def pull_feed(url, cache_path)
     @action_view = ActionView::Base.new(Rails::Configuration.new.view_path, {}, DummyController.new)    
-    url = "http://www.indymedia.org.uk/en/promotednewswire.rss" #args[:url]
     rss = SimpleRSS.parse(open(url))
     @feed_items = rss.items[0..9]
     output = ""
     @feed_items.each do |feed_item|
       output +=  @action_view.render :partial => "feeds/external/list_title", :locals => {:list_title => feed_item}
     end
-    rio(RAILS_ROOT + "/public/system/cache/uk_feed.rhtml") < output
+    rio(RAILS_ROOT + "/public/system/cache/" + cache_path) < output    
+  end
+  
+  # Pulls the feed of promoted articles from Indymedia UK.
+  #
+  def pull_uk_promoted_feed
+    pull_feed("http://www.indymedia.org.uk/en/promotednewswire.rss", "uk_feed.rhtml")
     rio(RAILS_ROOT + "/public/system/cache/index.html").delete
+  end
+  
+  # Pulls all defined group feeds
+  #
+  def pull_group_feeds
+    feeds = %w(http://www.indymedia.org.uk/en/newswire.rss http://www.indymedia.org.uk/en/bio-technology/newswire.rss)
+    count = 0
+    feeds.each do |feed|
+      pull_feed(feed, "feed#{count}.rhtml")
+      count = count + 1
+    end
   end
 
 end
 
-
+# A faked-out Controller class which is necessary so that we can use parts of Rails
+# ActionController infrastructure to render Rails views and write them to disk.
+# 
 class DummyController
 
   def logger

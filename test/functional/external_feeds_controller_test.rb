7c86e42a76cc7d4db17f068b0536870801a3b49e
yossarian <yossarian@aktivix.org>
2008-11-02 16:08:47 +0000

    All tests now passing for external feeds, still need to set up security and test model.

--- test/functional/external_feeds_controller_test.rb
+++ test/functional/external_feeds_controller_test.rb
@@ -1,35 +1,38 @@
-require 'test_helper'
+require 'test/test_helper'
 
 class ExternalFeedsControllerTest < ActionController::TestCase
 
+  fixtures :external_feeds, :collectives
+
   def test_should_get_new
-    get :new
+    get :new, :group_id => collectives(:indy_london).id
     assert_response :success
   end
 
   def test_should_create_external_feed
     assert_difference('ExternalFeed.count') do
-      post :create, :external_feed => { }
+      post :create,  {:external_feed => {:title=> "foo feed", :url => "http://www.indymedia.org.uk/en/promotednewswire.rss", :summary => "foo foo foo" }, :group_id => collectives(:indy_london).id}
     end
 
-    assert_redirected_to external_feed_path(assigns(:external_feed))
+    assert_redirected_to group_external_feeds_path(assigns(:group))
   end
 
   def test_should_get_edit
-    get :edit, :id => external_feeds(:one).id
+    get :edit, :id => external_feeds(:one).id, :group_id => collectives(:indy_london).id
     assert_response :success
   end
 
   def test_should_update_external_feed
-    put :update, :id => external_feeds(:one).id, :external_feed => { }
-    assert_redirected_to external_feed_path(assigns(:external_feed))
+    put :update, :group_id => collectives(:indy_london).id, :id => external_feeds(:one).id, :external_feed => { }
+    assert_redirected_to group_external_feeds_path(assigns(:group))
   end
 
   def test_should_destroy_external_feed
     assert_difference('ExternalFeed.count', -1) do
-      delete :destroy, :id => external_feeds(:one).id
+      delete :destroy, :id => external_feeds(:one).id, :group_id => collectives(:indy_london).id
     end
 
-    assert_redirected_to external_feeds_path
+    assert_redirected_to group_external_feeds_path(assigns(:group))
   end
+  
 end

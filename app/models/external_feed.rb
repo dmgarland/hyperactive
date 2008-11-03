d822a767e800dd656485dc3c3373ff5e00b94f63
yossarian <yossarian@aktivix.org>
2008-11-03 01:05:16 +0000

    Summaries should be limited to 255, not 50.

--- app/models/external_feed.rb
+++ app/models/external_feed.rb
@@ -4,7 +4,7 @@ class ExternalFeed < ActiveRecord::Base
   
   validates_presence_of :title, :url
   validates_length_of :title, :maximum => 50
-  validates_length_of :summary, :maximum => 50
+  validates_length_of :summary, :maximum => 255
   validates_uri_existence_of :url
   
   before_destroy :delete_cache_file

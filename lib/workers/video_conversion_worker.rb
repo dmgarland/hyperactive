3c1d59d1fa68bb22ab08f11bdecd4b5b66a8ea58
yossarian <yossarian@aktivix.org>
2008-11-15 21:53:46 +0000

    Removed some old useless code.

--- lib/workers/video_conversion_worker.rb
+++ lib/workers/video_conversion_worker.rb
@@ -5,12 +5,12 @@
 class VideoConversionWorker < BackgrounDRb::Rails
   
   require 'digest/sha1'
-  #require "#{RAILS_ROOT}/vendor/plugins/hyperactive/lib/rubytorrent/rubytorrent.rb"
   
   attr_accessor :video_file, :video_id
   
   # This method is called in it's own new thread when you
-  # call new worker. args is set to :args  
+  # call new worker. args is set to :args
+  #
   def do_work(args)
     @video_file = args[:absolute_path]
     @video_id = args[:video_id].to_i
@@ -24,8 +24,6 @@ class VideoConversionWorker < BackgrounDRb::Rails
     `nice -n +19 ffmpeg -i #{@video_file} -ss 00:00:05 -t 00:00:01 -vcodec mjpeg -vframes 1 -an -f rawvideo -s 320x240 #{@video_file}.jpg`
     `nice -n +19 ffmpeg -i #{@video_file} -ab 64 -ar 22050 -b 500000 -r 25 -s 320x240 #{@video_file}.flv`
     `nice -n +19 ffmpeg2theora #{@video_file}`
-    puts "files created"
-    # note: the creation of torrent metafiles depends on the transmissioncli package, install it into your OS through your package manager
     ogg_file = @video_file.chomp(File.extname(@video_file)) + ".ogg"
     `btmakemetafile.bittornado #{@torrent_tracker} #{ogg_file}`
     
@@ -42,69 +40,6 @@ class VideoConversionWorker < BackgrounDRb::Rails
       FileUtils.rm "#{RAILS_ROOT}/public/system/cache/featured_videos_json.html", :force => true   # never raises exception
     end
   end
-  
-  
-  # Constructs a torrent file.  For whatever reason, the generated files aren't usable
-  # by libtransmission, so this was all a big waste of time. I'll leave it in for
-  # the moment in case I can get it to work with further hacking.  The other option 
-  # would be to figure out how to extend the libtransmission gem wrapper to take
-  # advantage of libtransmission's ability to make metainfo files.
-  #
-  # To invoke it instead of the btmakemetafile line above, we'd use:
-  # torrent = construct_torrent("#{@video_file.chomp(File.extname(@video_file))}.ogg")
-  #
-  def construct_torrent(file, comment="")
-    files = file
-    mi = RubyTorrent::MetaInfo.new
-    mii = RubyTorrent::MetaInfoInfo.new
-    mii.name = file
-    mii.length = files.inject(0) { |s, f| s + File.size(f) }   
-    mii.piece_length = 32 * 1024
-    mii.pieces = ""
-    i = 0
-    read_pieces(files, mii.piece_length) do |piece|
-      mii.pieces += Digest::SHA1.digest(piece)
-      i += 1
-    end
-    mi.info = mii
-    tier = 1
-    trackers = [@torrent_tracker]
-    mi.announce = URI.parse(@torrent_tracker)
-    mi.announce_list = trackers.map do |tier|
-      tier.map { |x| URI.parse(x) }
-    end 
-    mi.comment = comment
-    mi.created_by = "RubyTorrent make-metainfo (http://rubytorrent.rubyforge.org)"
-    mi.creation_date = Time.now  
-    name = "#{mii.name}.torrent" 
-    File.open(name, "w") do |f|
-      f.write mi.to_bencoding
-    end
-    return name    
-  end
-  
-  
-  private
-  
-  # Reads the pieces of the file into a buffer
-  #
-  def read_pieces(files, length)
-    buf = ""
-    files.each do |f|
-      File.open(f) do |fh|
-        begin
-          read = fh.read(length - buf.length)
-          if (buf.length + read.length) == length
-            yield(buf + read)
-            buf = ""
-          else
-            buf += read
-          end
-        end until fh.eof?
-      end
-    end
-    yield buf
-  end   
 
 end
 

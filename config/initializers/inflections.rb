14669e6cef07fb4eb5d4e0314d59f171b76eb519
yossarian <yossarian@aktivix.org>
2008-11-24 01:57:21 +0000

    Ditching globalize config, getting inflections up to Rails 2.2.2 standards

--- config/initializers/inflections.rb
+++ config/initializers/inflections.rb
@@ -1,9 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
 # Add new inflection rules using the following format 
 # (all these examples are active by default):
-Inflector.inflections do |inflect|
+ActiveSupport::Inflector.inflections do |inflect|
 #   inflect.plural /^(ox)$/i, '\1en'
 #   inflect.singular /^(ox)en/i, '\1'
 #   inflect.irregular 'person', 'people'
-#   inflect.uncountable %w( fish sheep )
-  inflect.uncountable %w( content)
+   inflect.uncountable %w( content )
 end

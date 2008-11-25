8de15355b2b6a66ebcbc7ccdee543109b8bf5f7d
yossarian <yossarian@aktivix.org>
2008-11-25 00:44:51 +0000

    Killed some deprecation warnings.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -17,7 +17,7 @@ module ApplicationHelper
   def default_content_for(name, &block)
     name = name.kind_of?(Symbol) ? ":#{name}" : name
     out = eval("yield #{name}", block.binding)
-    concat(out || capture(&block), block.binding)
+    concat(out || capture(&block))
   end  
   
   def css_from_plugin?

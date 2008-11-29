9a27f561c573f9aeaf6766d2a73002f004c4a3c3
yossarian <yossarian@aktivix.org>
2008-11-29 20:16:31 +0000

    Association of ContentFilterExpression and ContentFilter works alright.

--- test/unit/content_filter_test.rb
+++ test/unit/content_filter_test.rb
@@ -8,7 +8,11 @@ class ContentFilterTest < ActiveSupport::TestCase
   end
   
   def test_association_of_content_filter_expressions
-    
+    content_filter = ContentFilter.new(:title => "foo", :summary => "bar")
+    content_filter_exp = ContentFilterExpression.new(:summary => "foo_exp", :regexp => "bar_exp")
+    content_filter.content_filter_expressions << content_filter_exp
+    content_filter.save!
+    assert_same content_filter.content_filter_expressions.first, content_filter_exp
   end
 
 end

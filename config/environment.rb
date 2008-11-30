892a704f4824e464d6f0dd01a006b1e983f6bcde
yossarian <yossarian@aktivix.org>
2008-11-30 13:43:09 +0000

    Taking the version requirement off of the will_paginate gem, it should work fine with any version so far.

--- config/environment.rb
+++ config/environment.rb
@@ -67,7 +67,7 @@ Rails::Initializer.run do |config|
   # Hyperactive plugin.
   #config.reload_plugins = true
 
-  config.gem 'mislav-will_paginate', :version => '2.3.4', :lib => 'will_paginate', :source => 'http://gems.github.com'
+  config.gem 'mislav-will_paginate', :lib => 'will_paginate', :source => 'http://gems.github.com'
   config.gem 'acts_as_ferret'
   config.gem 'ferret'
   config.gem 'daemons'

9a4f8806acb2a664a6f013ac56264c78dcd446d1
yossarian <yossarian@aktivix.org>
2008-12-03 23:31:06 +0000

    Removing useless config.plugins setting from staging environment.

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -18,8 +18,6 @@ config.action_controller.perform_caching             = true
 # config.action_mailer.raise_delivery_errors = false
 # config.action_mailer.delivery_method = :sendmail
 
-config.plugins = ["white_list", "sanitize_params", "engines", "*"]
-
 config.action_mailer.raise_delivery_errors = true
 
 ActionMailer::Base.delivery_method = :sendmail

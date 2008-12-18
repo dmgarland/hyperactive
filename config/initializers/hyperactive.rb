b36b44c1c2ddfead30e23d8d5c3a5a1b62a7d8b0
yossarian <yossarian@aktivix.org>
2008-12-18 22:23:52 +0000

    Implemented a XapianIndexWorker to update search indexes once an hour or so.

--- config/initializers/hyperactive.rb
+++ config/initializers/hyperactive.rb
@@ -21,3 +21,10 @@ require 'vpim/icalendar'
 ActiveRbac.controller_layout = "admin" 
 
 ROUTES_PROTOCOL = (ENV["RAILS_ENV"] =~ /development/ ? "http://" : "https://")
+
+# The path to rake on this system (usually on a Debian system it'll be at
+# /usr/bin/rake).  This is used by the XapianIndexWorker so it can happily
+# run rake when it needs to update search indexes. Usually there won't be
+# any need to change this, but it might be in a different place on your system.
+#
+RAKE_PATH = "/usr/bin/rake"
---
+++ lib/workers/xapian_index_worker.rb
@@ -0,0 +1,13 @@
+# Periodically runs the Xapian index update task so that our
+# search indexes stay up to date.
+#
+class XapianIndexWorker < BackgrounDRb::Rails
+  
+  repeat_every 60.minutes
+  first_run Time.now + 30 # Let's give it 30 seconds for everything else to get warmed up
+  
+  def do_work(args)
+    `cd #{RAILS_ROOT} && RAILS_ENV=#{RAILS_ENV} #{RAKE_PATH} xapian:update_index`
+  end
+
+end

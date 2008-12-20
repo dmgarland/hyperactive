74bfb0c04a552b9c52532bcf22fbc13e9855e13a
yossarian <yossarian@aktivix.org>
2008-12-20 00:48:56 +0000

    Overhauled and modernized the Admin::PagesController, which was generated with scaffold a loooooooooooongg time ago.

--- app/controllers/admin/pages_controller.rb
+++ app/controllers/admin/pages_controller.rb
@@ -1,10 +1,16 @@
 class Admin::PagesController < ApplicationController
 
+  layout "admin"
+
+  # Filters
+  #
+  before_filter :protect_controller
+
+  # Macros
+  #
   include SslRequirement
   ssl_required :all
-
   cache_sweeper :content_sweeper, :only => [:create, :update, :destroy]
-
   uses_tiny_mce(:options => {:theme => 'advanced',
                            :browsers => %w{msie gecko safari opera},
                            :theme_advanced_toolbar_location => "top",
@@ -20,12 +26,6 @@ class Admin::PagesController < ApplicationController
                            :plugins => %w{paste},
                            :valid_elements => Hyperactive.valid_elements_for_tiny_mce},
               :only => [:new, :edit, :create, :update])   
-
-  layout "admin"
-
-  # GETs should be safe (see http://www.w3.org/2001/tag/doc/whenToUseGet.html)
-  verify :method => :post, :only => [ :destroy, :create, :update ],
-         :redirect_to => { :action => :list }
 
   def index
     @pages = Page.paginate(
@@ -45,7 +45,7 @@ class Admin::PagesController < ApplicationController
     @page = Page.new(params[:page])
     if @page.save
       flash[:notice] = 'Page was successfully created.'
-      redirect_to :action => 'list'
+      redirect_to admin_page_path(@page)
     else
       render :action => 'new'
     end
@@ -59,7 +59,7 @@ class Admin::PagesController < ApplicationController
     @page = Page.find(params[:id])
     if @page.update_attributes(params[:page])
       flash[:notice] = 'Page was successfully updated.'
-      redirect_to :action => 'show', :id => @page
+      redirect_to admin_page_path(@page)
     else
       render :action => 'edit'
     end
@@ -70,10 +70,10 @@ class Admin::PagesController < ApplicationController
     redirect_to :action => 'list'
   end
   
-  before_filter :protect_controller
-  
+  protected
+    
   def protect_controller
-    if !current_user.nil? and current_user.has_role?("Admin")
+    if current_user.has_role?("Admin")
       return true
     else
       security_error

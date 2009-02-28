195a3949b23a9152ad506f41e12954d166edf7e3
yossarian <yossarian@aktivix.org>
2009-02-28 20:40:13 +0000

    The auto-moderation stuff sends too many emails when something gets found, this should fix it.

--- lib/workers/auto_moderation_worker.rb
+++ lib/workers/auto_moderation_worker.rb
@@ -10,16 +10,18 @@ class AutoModerationWorker < BackgrounDRb::Rails
     fields_to_check = ["title", "body", "summary", "published_by"]
     content_to_check = Content.find(:all, :conditions => ["auto_moderation_checked = ?", false])
     content_to_check.each do |content|
+    found_something_to_hide = false
       fields_to_check.each do |field|
         filters.each do |filter|
           filter.content_filter_expressions.each do |filter_expression|
             if content.send(field) =~ /#{filter_expression.regexp}/i
               content.moderation_status = "hidden"
-              ContentHideMailer.deliver_hide(content, filter.summary, "marcos")
+              found_something_to_hide = true    
             end
           end
         end
       end
+      ContentHideMailer.deliver_hide(content, filter.summary, "marcos") if found_something_to_hide     
       content.auto_moderation_checked = true
       content.save!
     end

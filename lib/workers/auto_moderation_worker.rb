dec876bd07cb2a1b0c971782d78d16fd4f6bd8c4
yossarian <yossarian@aktivix.org>
2009-03-01 17:11:05 +0000

    Fixing moderation worker after screwing it all up.

--- lib/workers/auto_moderation_worker.rb
+++ lib/workers/auto_moderation_worker.rb
@@ -10,18 +10,20 @@ class AutoModerationWorker < BackgrounDRb::Rails
     fields_to_check = ["title", "body", "summary", "published_by"]
     content_to_check = Content.find(:all, :conditions => ["auto_moderation_checked = ?", false])
     content_to_check.each do |content|
-      found_something_to_hide = false
+      @found_something_to_hide = false
+      @filter_message = ""
       fields_to_check.each do |field|
         filters.each do |filter|
           filter.content_filter_expressions.each do |filter_expression|
             if content.send(field) =~ /#{filter_expression.regexp}/i
               content.moderation_status = "hidden"
-              found_something_to_hide = true    
+              @found_something_to_hide = true
+              @filter_message = filter.summary
             end
           end
         end
       end
-      ContentHideMailer.deliver_hide(content, filter.summary, "marcos") if found_something_to_hide     
+      ContentHideMailer.deliver_hide(content, @filter_message, "marcos") if found_something_to_hide     
       content.auto_moderation_checked = true
       content.save!
     end

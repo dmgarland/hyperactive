ad7d9082ca75ddac5610f49af56697b2b908e5d6
mish <mish@aktivix.org>
2009-05-08 16:30:41 +0000

    archive: refactored code into helper class, tests: altered fixtures as first stage

--- app/helpers/archive_helper.rb
+++ app/helpers/archive_helper.rb
@@ -20,5 +20,33 @@ module ArchiveHelper
   def render_archive_place_tag_list(article)
     render(:partial => "shared/archive_tag_list", :object=> article.place_tag_list, :locals => {:tag_type => "place"})
   end
+
+  def prev_month_link(start_date, type)
+    prev_month = start_date << 1
+    link_to "<< " + prev_month.strftime('%B %Y'), 
+            :action => "month_index", 
+            :year => prev_month.year, 
+            :month => prev_month.month, 
+            :type => type 
+  end
+
+  def next_month_link(start_date, type)
+    next_month = start_date >> 1
+    link_to next_month.strftime('%B %Y') + " >>",
+            :action => "month_index", 
+            :year => next_month.year, 
+            :month => next_month.month, 
+            :type => type 
+  end
+
+  def prev_year_link(year)
+    prev_year = year - 1
+    link_to "<< " + prev_year.to_s, :action => "year_index", :year => prev_year
+  end
+  
+  def next_year_link(year)
+    next_year = year + 1
+    link_to next_year.to_s + " >>", :action => "year_index", :year => next_year
+  end
   
 end

6558337f1bff9244cac7b81a856ab36be72e1a98
yossarian <yossarian@caprica.(none)>
2009-06-21 17:02:04 +0100

    Switched the rosette which everyone hated with the blue star which will provide a new focus for argument.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -13,52 +13,52 @@ module ApplicationHelper
       yield tag.name, style_list[(tag.popularity.to_i - min) / divisor]
     end
   end
-  
+
   def default_content_for(name, &block)
     name = name.kind_of?(Symbol) ? ":#{name}" : name
     out = eval("yield #{name}", block.binding)
     concat(out || capture(&block))
-  end  
-  
+  end
+
   def css_from_plugin?
     if Hyperactive.use_local_css
       return nil
     else
       return 'hyperactive'
     end
-  end  
-  
+  end
+
   # Create a link to a tag's view page.
   def tag_item_path
     "/search/by_tag/"
   end
-  
+
   def place_tag_item_path
     "/search/by_place_tag/"
   end
-  
+
   def long_date(date)
     "#{date.strftime('%A %d %B %G %H:%M')}"
-  end  
-  
-  
-  # Builds navigation tabs 
+  end
+
+
+  # Builds navigation tabs
   def tab_for(text, link, conditions = {})
     on = false
     if(!conditions[:controller_name].nil?)
       if(!conditions[:controller_name].nil?  && !conditions[:action_name].nil?)
         on = (@current_controller == conditions[:controller_name] && @current_action == conditions[:action_name])
-      else 
+      else
         on = (@current_controller == conditions[:controller_name])
-      end        
-    end    
-    
+      end
+    end
+
     if(!conditions[:slug].nil?)
       on = (params[:slug] == conditions[:slug])
     end
-  
+
     content_tag :li, link_to(text, link), :class => ("current_page_item" if on), :id => "#{text.downcase.gsub(/ /, "")}-tab"
-  end    
+  end
 
   def content_path_for(entity)
     if entity.class == Article
@@ -71,7 +71,7 @@ module ApplicationHelper
       other_media_path(entity)
     end
   end
-  
+
   def icon_image_for(entity)
     if entity.class == Article
       image_tag "icon_article.gif"
@@ -80,19 +80,19 @@ module ApplicationHelper
     elsif entity.class == Video
       image_tag "icon_video.gif"
     elsif entity.class == OtherMedia
-      image_tag "icons/world_link.png"  
-    end    
-  end  
-  
+      image_tag "icons/world_link.png"
+    end
+  end
+
   def moderation_status_icon_for(entity)
     if entity.moderation_status == "featured"
       image_tag "icons/star.png"
     elsif entity.moderation_status == "promoted"
-      image_tag "icons/rosette.png"
-    end    
-  end    
+      image_tag "icons/star-blue1.png"
+    end
+  end
 
-  # A convenience method giving access to the thumbnail for this 
+  # A convenience method giving access to the thumbnail for this
   # content object.
   #
   def thumbnail_for(entity)
@@ -106,29 +106,29 @@ module ApplicationHelper
         elsif entity.contains_videos?
           video = entity.videos.first
           video_thumb = video.file.url
-          link_to(image_tag(video_thumb + ".small.jpg", :class => 'left'), entity)          
+          link_to(image_tag(video_thumb + ".small.jpg", :class => 'left'), entity)
         end
       end
     end
-  end  
-  
+  end
+
   def render_tag_list
     render(:partial => "shared/tag_list", :object=> @content.tag_list, :locals => {:tag_type => "tag"})
   end
-  
+
   def render_place_tag_list
     render(:partial => "shared/tag_list", :object=> @content.place_tag_list, :locals => {:tag_type => "place"})
   end
-  
+
   def page_links(list)
     render(:partial => "shared/pagination", :object => list)
   end
-  
-  
+
+
   # Same as thumbnail for but in a somewhat larger size.  Note that videos only
-  # have one thumbnail size, so this will return the same thing as thumbnail_for 
+  # have one thumbnail size, so this will return the same thing as thumbnail_for
   # for a video.
-  # 
+  #
   def big_thumbnail_for(entity)
     if entity.class == Video
       video_thumb = entity.file
@@ -140,16 +140,16 @@ module ApplicationHelper
         elsif entity.contains_videos?
           video = entity.videos.first
           video_thumb = video.file
-          link_to(image_tag(video_thumb + ".small.jpg", :class => 'left'), entity)          
+          link_to(image_tag(video_thumb + ".small.jpg", :class => 'left'), entity)
         end
       end
     end
-  end  
-  
-  def collective_link_to(collective)
-    render :partial => "shared/collectives/collective_link", :object => collective  
   end
-  
+
+  def collective_link_to(collective)
+    render :partial => "shared/collectives/collective_link", :object => collective
+  end
+
   def render_subfeature(index)
     unless (@top_featured_articles.nil? || @top_featured_articles[index].nil?)
       render :partial => "home/subfeature", :locals => {:subfeature => @top_featured_articles[index]}
@@ -165,8 +165,8 @@ module ApplicationHelper
       end
     end
     return nil
-  end    
-  
+  end
+
   def most_recent_promoted_event_with_image
     unless @featured_events.nil?
       @featured_events.each do |event|
@@ -176,10 +176,10 @@ module ApplicationHelper
       end
     end
     return nil
-  end    
-  
+  end
+
   def downcased_class_name(content)
-    case content.class.to_s.humanize.downcase 
+    case content.class.to_s.humanize.downcase
       when 'article'
         (I18n.t 'content.class_names.article').downcase
       when 'event'
@@ -190,7 +190,8 @@ module ApplicationHelper
         (I18n.t 'content.class_name.comment').downcase
       when 'other media'
         (I18n.t 'content.class_names.other_media').downcase
-    end 
+    end
   end
-  
+
 end
+

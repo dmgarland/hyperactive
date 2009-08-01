9b6146bb51dfb2054886f41630edfc2a1e195265
yossarian <yossarian@aktivix.org>
2009-08-02 00:06:24 +0100

    Seeing if this fixes exception notification or what.

--- config/initializers/exception_notifier.rb
+++ config/initializers/exception_notifier.rb
@@ -2,6 +2,7 @@
 #
 unless RAILS_ENV == 'test'
   ExceptionNotifier.exception_recipients = %w(yossarian@aktivix.org)
-  ExceptionNotifier.sender_address = %("Application Error" <error@london.indymedia.org.uk>)
+  ExceptionNotifier.sender_address = "error@london.indymedia.org.uk"
   ExceptionNotifier.email_prefix = "[Hyperactive] "
 end
+

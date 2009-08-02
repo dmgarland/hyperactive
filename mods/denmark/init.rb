43758ffb9a0ce159676d40aaa63ca27740b66a38
yossarian <yossarian@aktivix.org>
2009-08-02 23:11:43 +0100

    It's now possible to override locale files in mods.  Fixes #5767.

--- mods/denmark/init.rb
+++ mods/denmark/init.rb
@@ -3,3 +3,10 @@
 self.override_views = true
 #self.load_once = false
 
+# Override the standard locale files by dropping your own translations into
+# config/locales/xx.yml
+#
+Dir[File.join("#{File.dirname(__FILE__)}/config/locales/*.yml")].each do |locale|
+  I18n.load_path << locale
+end
+
